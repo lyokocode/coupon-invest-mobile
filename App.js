93d8e42af06485f114149aa0a0086bbf536aa867
lyokocode <aelita.dev@gmail.com>
2024-05-16 18:39:26 +0300

    auth control with redux toolkit

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
 import Screens from "./src/Screens"
 import { SafeAreaView } from 'react-native';
-
+import store from './src/store';
+import { Provider } from 'react-redux'
 export default function App() {
   return (
-    <NavigationContainer>
+    <Provider store={store}>
       <SafeAreaView style={{ flex: 1 }}>
-        <Screens />
+        <NavigationContainer>
+          <Screens />
+        </NavigationContainer>
       </SafeAreaView>
-    </NavigationContainer>
+    </Provider>
   );
 }
 
--- src/Screens.js
+++ src/Screens.js
@@ -1,54 +1,73 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Explore, Home, Favorite } from "./screens"
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useSelector } from 'react-redux';
+import { Explore, Home, Favorite, Login, Register } from "./screens"
 import { GlobeAltIcon, HeartIcon, HomeIcon } from 'react-native-heroicons/outline';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator()
 
 function Screens() {
+
+    const { auth } = useSelector(state => state.auth)
     return (
-        <Tab.Navigator
-            screenOptions={{
-                headerShown: false,
-                tabBarShowLabel: false,
-                tabBarActiveTintColor: "#22c55e",
-                tabBarInactiveTintColor: "#27272a",
-                tabBarStyle: {
-                    borderWidth: 0,
-                }
-            }}
-        >
-            <Tab.Screen name="Anasayfa" component={Home}
-                options={{
-                    tabBarIcon: ({ focused, color }) => {
-                        if (focused) {
-                            return <HomeIcon size={30} color={color} />
+        <>
+            {auth ? (
+
+                <Tab.Navigator
+                    screenOptions={{
+                        headerShown: false,
+                        tabBarShowLabel: false,
+                        tabBarActiveTintColor: "#22c55e",
+                        tabBarInactiveTintColor: "#27272a",
+                        tabBarStyle: {
+                            borderWidth: 0,
                         }
-                        return <HomeIcon size={30} color={color} />
-                    }
-                }}
-            />
-            <Tab.Screen name="Keşfet" component={Explore}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ focused, color }) => {
-                        if (focused) {
-                            return <GlobeAltIcon size={30} color={color} />
-                        }
-                        return <GlobeAltIcon size={30} color={color} />
-                    }
-                }}
-            />
-            <Tab.Screen name="Favoriler" component={Favorite}
-                options={{
-                    tabBarIcon: ({ focused, color }) => {
-                        if (focused) {
-                            return <HeartIcon size={32} color="red" />
-                        }
-                        return <HeartIcon size={30} color={color} />
-                    }
-                }}
-            />
-        </Tab.Navigator>
+                    }}
+                >
+                    <Tab.Screen name="Anasayfa" component={Home}
+                        options={{
+                            tabBarIcon: ({ focused, color }) => {
+                                if (focused) {
+                                    return <HomeIcon size={30} color={color} />
+                                }
+                                return <HomeIcon size={30} color={color} />
+                            }
+                        }}
+                    />
+                    <Tab.Screen name="Keşfet" component={Explore}
+                        options={{
+                            headerShown: false,
+                            tabBarIcon: ({ focused, color }) => {
+                                if (focused) {
+                                    return <GlobeAltIcon size={30} color={color} />
+                                }
+                                return <GlobeAltIcon size={30} color={color} />
+                            }
+                        }}
+                    />
+                    <Tab.Screen name="Favoriler" component={Favorite}
+                        options={{
+                            tabBarIcon: ({ focused, color }) => {
+                                if (focused) {
+                                    return <HeartIcon size={32} color="red" />
+                                }
+                                return <HeartIcon size={30} color={color} />
+                            }
+                        }}
+                    />
+                </Tab.Navigator>
+            ) : (
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false
+                    }}
+                >
+                    <Stack.Screen name="Login" component={Login} />
+                    <Stack.Screen name="Register" component={Register} />
+                </Stack.Navigator>
+            )}
+        </>
     )
 
 }
